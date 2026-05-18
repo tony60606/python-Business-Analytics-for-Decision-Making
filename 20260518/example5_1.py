904f637baa8781ecaba6cd029cfbb4dda27d6566
tony60606 <ttoonnyy9@gmail.com>
2026-05-18 22:05:36 +0800

    Add files via upload

---
+++ 20260518/example5_1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from common import factor
+pd.set_option('display.max.columns',None)
+facts=factor()
+by_payment_dist=facts.groupby('payment_type',as_index=False).agg(
+    平均客單價=('line_revenue','mean'),
+    中位數=('line_revenue','median'),
+    P90=('line_revenue',lambda x : x.quantile(0.9)) ,
+    標準差=('line_revenue','std')
+).round(2)
+by_payment_dist.to_csv('temp1.csv')
+print(by_payment_dist.sort_values("P90",ascending=False))
